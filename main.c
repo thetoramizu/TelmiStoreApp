336b609d686322b3eb0ab5a12d7bc7db164fa044
thetoramizu <38066800+thetoramizu@users.noreply.github.com>
2026-09-12 13:21:38 +0000

    sd

--- main.c
+++ main.c
@@ -2,12 +2,30 @@
 #include <stdio.h>
 
 
+#define WIDTH 640
+#define HEIGHT 480
+
+
+typedef struct
+{
+    const char *title;
+    const char *zip;
+} Story;
+
+
+Story stories[] =
+{
+    {"La grande aventure spatiale", "giglaxe.zip"},
+    {"Voyage en terre ocre", "mpa.zip"}
+};
+
+
 int main(int argc, char *argv[])
 {
     printf("TelmiStore SDL start\n");
 
 
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) != 0)
     {
         printf("SDL init erreur: %s\n", SDL_GetError());
         return 1;
@@ -18,15 +36,15 @@ int main(int argc, char *argv[])
         "TelmiStore",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        640,
-        480,
+        WIDTH,
+        HEIGHT,
         0
     );
 
 
     if (!window)
     {
-        printf("Erreur fenetre: %s\n", SDL_GetError());
+        printf("Fenetre erreur: %s\n", SDL_GetError());
         return 1;
     }
 
@@ -35,34 +53,139 @@ int main(int argc, char *argv[])
         SDL_CreateRenderer(window, -1, 0);
 
 
+    if (!renderer)
+    {
+        printf("Renderer erreur: %s\n", SDL_GetError());
+        return 1;
+    }
+
+SDL_Joystick *joystick = NULL;
+
+if(SDL_NumJoysticks() > 0)
+{
+    joystick = SDL_JoystickOpen(0);
+
+    if(joystick)
+        printf("Joystick OK\n");
+    else
+        printf("Erreur joystick: %s\n", SDL_GetError());
+}
+else
+{
+    printf("Pas de joystick\n");
+}
+
+
+    int selected = 0;
     int running = 1;
+
 
     while(running)
     {
         SDL_Event e;
 
+
         while(SDL_PollEvent(&e))
         {
+
             if(e.type == SDL_QUIT)
                 running = 0;
 
-            if(e.type == SDL_KEYDOWN)
+
+            if(e.type == SDL_JOYBUTTONDOWN)
             {
-                if(e.key.keysym.sym == SDLK_ESCAPE)
+                printf("Bouton %d\n", e.jbutton.button);
+
+
+                // Haut
+                if(e.jbutton.button == 4)
+                {
+                    selected--;
+                    if(selected < 0)
+                        selected = 1;
+                }
+
+
+                // Bas
+                if(e.jbutton.button == 5)
+                {
+                    selected++;
+                    if(selected > 1)
+                        selected = 0;
+                }
+
+
+                // A
+                if(e.jbutton.button == 0)
+                {
+                    printf(
+                        "Selection : %s\n",
+                        stories[selected].title
+                    );
+                }
+
+
+                // B
+                if(e.jbutton.button == 1)
+                {
                     running = 0;
+                }
             }
         }
 
 
+        // fond
         SDL_SetRenderDrawColor(
             renderer,
-            0,
-            0,
-            0,
+            20,
+            20,
+            30,
             255
         );
 
         SDL_RenderClear(renderer);
+
+
+        // cases histoires
+        for(int i=0;i<2;i++)
+        {
+
+            SDL_Rect box;
+
+            box.x = 50;
+            box.y = 80 + i*100;
+            box.w = 540;
+            box.h = 70;
+
+
+            if(i == selected)
+            {
+                SDL_SetRenderDrawColor(
+                    renderer,
+                    80,
+                    80,
+                    200,
+                    255
+                );
+            }
+            else
+            {
+                SDL_SetRenderDrawColor(
+                    renderer,
+                    50,
+                    50,
+                    60,
+                    255
+                );
+            }
+
+
+            SDL_RenderFillRect(
+                renderer,
+                &box
+            );
+        }
+
 
         SDL_RenderPresent(renderer);
 
@@ -70,10 +193,16 @@ int main(int argc, char *argv[])
     }
 
 
+if(joystick)
+{
+    SDL_JoystickClose(joystick);
+}
+
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 
     SDL_Quit();
+
 
     return 0;
 }
