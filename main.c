2cf83de66dced5938e3351a7085146e38f4f1e0e
thetoramizu <38066800+thetoramizu@users.noreply.github.com>
2026-09-12 13:39:08 +0000

    sdq

--- main.c
+++ main.c
@@ -1,39 +1,76 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <time.h>
 
 
 #define WIDTH 640
 #define HEIGHT 480
 
 
+FILE *logfile;
+
+
+void logmsg(const char *msg)
+{
+    if(logfile)
+    {
+        fprintf(logfile, "%s\n", msg);
+        fflush(logfile);
+    }
+
+    printf("%s\n", msg);
+}
+
+
 int main(int argc, char *argv[])
 {
-    printf("TelmiStore SDL TEST start\n");
+    logfile = fopen("telmistore.log", "w");
+
+    logmsg("TelmiStore SDL TEST start");
 
 
     if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) != 0)
     {
-        printf("SDL init erreur: %s\n", SDL_GetError());
+        logmsg(SDL_GetError());
         return 1;
     }
 
 
+    char buffer[256];
+
+
+    snprintf(
+        buffer,
+        sizeof(buffer),
+        "Video driver: %s",
+        SDL_GetCurrentVideoDriver()
+    );
+
+    logmsg(buffer);
+
+
+
     SDL_Window *window = SDL_CreateWindow(
-        "TelmiStore TEST",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
+        "TelmiStore",
+        0,
+        0,
         WIDTH,
         HEIGHT,
-        0
+        SDL_WINDOW_SHOWN
     );
 
 
     if(!window)
     {
-        printf("Erreur fenetre: %s\n", SDL_GetError());
+        logmsg("Erreur SDL_CreateWindow");
+        logmsg(SDL_GetError());
         SDL_Quit();
         return 1;
     }
+
+
+    logmsg("Window OK");
+
 
 
     SDL_Renderer *renderer =
@@ -46,11 +83,16 @@ int main(int argc, char *argv[])
 
     if(!renderer)
     {
-        printf("Erreur renderer: %s\n", SDL_GetError());
+        logmsg("Erreur renderer");
+        logmsg(SDL_GetError());
         SDL_DestroyWindow(window);
         SDL_Quit();
         return 1;
     }
+
+
+    logmsg("Renderer OK");
+
 
 
     SDL_Joystick *joystick = NULL;
@@ -61,63 +103,104 @@ int main(int argc, char *argv[])
         joystick = SDL_JoystickOpen(0);
 
         if(joystick)
-            printf("Joystick OK\n");
+            logmsg("Joystick OK");
         else
-            printf("Erreur joystick: %s\n", SDL_GetError());
+            logmsg("Erreur joystick");
     }
     else
     {
-        printf("Pas de joystick\n");
+        logmsg("Pas de joystick");
     }
+
 
 
     int running = 1;
 
+    Uint32 start = SDL_GetTicks();
+
+
 
     while(running)
     {
+
         SDL_Event e;
 
 
         while(SDL_PollEvent(&e))
         {
+
             if(e.type == SDL_QUIT)
+            {
+                logmsg("SDL_QUIT");
                 running = 0;
+            }
+
 
 
             if(e.type == SDL_KEYDOWN)
             {
-                printf("Key %d\n", e.key.keysym.sym);
+                snprintf(
+                    buffer,
+                    sizeof(buffer),
+                    "KEY %d",
+                    e.key.keysym.sym
+                );
+
+                logmsg(buffer);
+
 
                 if(e.key.keysym.sym == SDLK_ESCAPE)
                     running = 0;
             }
 
 
+
             if(e.type == SDL_JOYBUTTONDOWN)
             {
-                printf("Joy button %d\n", e.jbutton.button);
+                snprintf(
+                    buffer,
+                    sizeof(buffer),
+                    "JOY BUTTON %d",
+                    e.jbutton.button
+                );
 
-                // B
-                if(e.jbutton.button == 1)
+                logmsg(buffer);
+
+
+                // B ou START selon mapping Onion
+                if(e.jbutton.button == 1 ||
+                   e.jbutton.button == 9)
+                {
                     running = 0;
+                }
             }
         }
 
 
-        // fond bleu foncé
+
+        // sécurité : fermeture après 30 secondes
+        if(SDL_GetTicks() - start > 30000)
+        {
+            logmsg("Timeout 30s");
+            running = 0;
+        }
+
+
+
+        // fond bleu
         SDL_SetRenderDrawColor(
             renderer,
             20,
             40,
-            80,
+            100,
             255
         );
 
         SDL_RenderClear(renderer);
 
 
-        // rectangle rouge
+
+        // carré rouge
         SDL_Rect r1 =
         {
             100,
@@ -141,7 +224,8 @@ int main(int argc, char *argv[])
         );
 
 
-        // rectangle vert
+
+        // carré vert
         SDL_Rect r2 =
         {
             340,
@@ -165,6 +249,7 @@ int main(int argc, char *argv[])
         );
 
 
+
         SDL_RenderPresent(renderer);
 
 
@@ -172,7 +257,8 @@ int main(int argc, char *argv[])
     }
 
 
-    printf("Fermeture TelmiStore\n");
+
+    logmsg("Closing");
 
 
     if(joystick)
@@ -182,7 +268,12 @@ int main(int argc, char *argv[])
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 
+
     SDL_Quit();
+
+
+    if(logfile)
+        fclose(logfile);
 
 
     return 0;
